bff3d61e82846c531b83d85033312e883edfafbe
Carlos Zambrano Mendoza <carlosz@planb.ec>
2018-07-08 18:16:03 -0500

    HTMLS del sitio

--- contactos.php
+++ contactos.php
@@ -20,9 +20,10 @@
 			<div class="container page-content">
 
 				<div class="col-right">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1185.343961273957!2d-79.90679446053717!3d-2.151407717654847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x516eabf4c058c0a8!2sQuality+of+Care!5e0!3m2!1ses-419!2s!4v1472854845890" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-					<p><strong>Direcci&oacute;n:</strong> Centro empresarial Ciudad Col&oacute;n, Edf. Empresarial 1, 2do piso Of 208<br>
-					<strong>Telfs.:</strong> (593-4) 2136748<br>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1185.343961273957!2d-79.90679446053717!3d-2.151407717654847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x516eabf4c058c0a8!2sQuality+of+Care!5e0!3m2!1ses-419!2s!4v1472854845890" width="600" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<p><strong>Direcci&oacute;n:</strong> Av. Joaquín Orrantia y Av. Juan Tanca Marengo Edificio Equilibrium, Piso 4, Of 417<br>GUAYAQUIL, ECUADOR<br>
+					<strong>Telfs.:</strong> (593-4) 042109180 Ext 129<br>
+
 					<strong>E-mail:</strong> <a href="mailto:info@gastritech.com.ec">info@gastritech.com.ec</a></p>
 				</div>
 
---
+++ quienes-somos.php
@@ -0,0 +1,50 @@
+﻿<?php include ("code-head.php"); ?>
+
+<body>
+<?php include("header.php"); ?>
+	
+	<main id="content-area">
+		<div class="bg-cont">
+	
+		<article class="single-page">
+			
+			
+			<header class="page-header">
+				<div class="container">		
+					<h1>Quienes somos</h1>					
+				</div>
+			</header><!-- /.page-header -->
+			
+			
+			<div class="container page-content-min">
+
+				<section class="noticias"><!--Noticia 1 -->		
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempor scelerisque fringilla. Duis tincidunt fermentum consectetur. Pellentesque sem sapien, bibendum a ipsum vel, porta suscipit tellus. Nunc sodales, erat vitae ultricies molestie, libero orci pretium sapien, vitae interdum tortor urna in nisl. Vivamus sagittis urna eget faucibus hendrerit. Pellentesque viverra tristique tellus vitae ornare. Praesent mi felis, dignissim ac est in, dapibus aliquam ligula. Donec convallis libero nec varius porta. Integer dignissim vulputate tortor nec tincidunt. Etiam ipsum sem, lacinia sit amet cursus sed, eleifend in est. Ut congue vel purus sit amet consectetur.</p>
+				</section>
+
+				<section class="noticias"><!--Noticia 2 -->		
+					<figure class="foto"><img src="img/foto_muebles.jpg"></figure>
+					<h3>Misión</h3>
+					<p>Nunc sodales, erat vitae ultricies molestie, libero orci pretium sapien, vitae interdum tortor urna in nisl. Vivamus sagittis urna eget faucibus hendrerit. Pellentesque viverra tristique tellus vitae ornare.</p>
+				</section>
+
+				<section class="noticias"><!--Noticia 3 -->		
+					<figure class="foto"><img src="img/foto_muebles.jpg"></figure>
+					<h3>Visión</h3>
+					<p>Integer dignissim vulputate tortor nec tincidunt. Etiam ipsum sem, lacinia sit amet cursus sed, eleifend in est. Ut congue vel purus sit amet consectetur.</p>
+				</section>
+
+
+
+			</div><!-- /.content -->
+			
+			</article><!-- /#single-page -->
+		</div>
+	</main><!-- /#content-area -->
+	
+	<!-- Livemotive -->
+	
+<?php include("footer.php"); ?>
+	
+</body>
+</html>
